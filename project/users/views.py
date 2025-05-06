9a016a3edd790b15b5d05198eff89f0618d7c336
Abdul Kalam Azad <abdulkalamazad09092002@gmail.com>
2025-05-06 17:50:52 +0530

    updated

--- project/users/views.py
+++ project/users/views.py
@@ -22,7 +22,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('profile')
+            return redirect('app:sales_list')
     return render(request, 'users/login.html')
 
 def logout_view(request):
